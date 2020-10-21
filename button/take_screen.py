344a34e3a631e33b4c966419ba43e851e56fa1c9
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-10-22 01:06:36 +0200

    Take screenshot

---
+++ button/take_screen.py
@@ -0,0 +1,20 @@
+import os
+# from signal import pause
+from gpiozero import Button
+from datetime import datetime
+
+def take_screen():
+    screen_btn = Button(2)
+    
+    while True:
+    if screen_btn.is_pressed:
+        timestamp = datetime.now()
+        cmd = 'scrot -up -d 1 $n {}.png'.format('screen_' + str(timestamp))
+        os.system(cmd)
+    
+    #screen_btn.when_pressed=os.system(cmd)
+    #pause()
+    
+
+take_screen()    
+    
