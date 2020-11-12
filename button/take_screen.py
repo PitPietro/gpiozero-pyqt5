22472ce3c381a00f27d6c9e25c214495e9509a12
Pit RPI3 <pit.pietro@protonmail.com>
2020-11-12 09:48:06 +0100

    take screen

--- button/take_screen.py
+++ button/take_screen.py
@@ -7,10 +7,10 @@ def take_screen():
     screen_btn = Button(2)
     
     while True:
-    if screen_btn.is_pressed:
-        timestamp = datetime.now()
-        cmd = 'scrot -up -d 1 $n {}.png'.format('screen_' + str(timestamp))
-        os.system(cmd)
+        if screen_btn.is_pressed:
+            timestamp = datetime.now()
+            cmd = "scrot -u d 5 $n {}.png".format('screen_' + str(timestamp))
+            os.system(cmd)
     
     #screen_btn.when_pressed=os.system(cmd)
     #pause()
